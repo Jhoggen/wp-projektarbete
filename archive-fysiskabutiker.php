7e90250d6af12df43609bcebba99c0539c9fdeff
Jhoggen <jacob.hoggen@medieinstitutet.se>
2022-04-22 09:17:19 +0200

    fixed cpt landing page

--- archive-fysiskabutiker.php
+++ archive-fysiskabutiker.php
@@ -4,7 +4,8 @@
  if ( have_posts() ) {    
             
     while ( have_posts() ) {    
-         the_post(); ?>          
+         the_post();
+         the_title(); ?>          
          <div class="g-col-6 g-col-md-4">     
                  <?php the_content(); ?>    
          </div>    
